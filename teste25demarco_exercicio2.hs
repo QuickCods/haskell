7847fe996734497bcdb2729ecac75ba6bdf05af4
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-04-14 13:35:07 +0100

    Update teste25demarco_exercicio2.hs

--- teste25demarco_exercicio2.hs
+++ teste25demarco_exercicio2.hs
@@ -1,4 +1,4 @@
  numEqual :: Eq a => a -> a -> a -> Int               --ou  numEqual :: Int -> Int -> Int -> Int
  numEqual n m p | n == m && m == p = 3
- 	            | n == m || m == p || n == p = 2
- 	            | otherwise = 0
+ 	              | n == m || m == p || n == p = 2
+ 	              | otherwise = 0
