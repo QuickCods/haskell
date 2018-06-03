6295afe7df5da3fbf73a14678fe0e81719862d03
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-06-03 23:49:20 +0100

    Create exercicio43.hs

---
+++ exercicio43.hs
@@ -0,0 +1,12 @@
+ -- a)
+ 
+ insert :: Ord a => a -> [a] -> [a]
+ insert x []     = [x]
+ insert x (y:ys) | x <= y = (x:y:ys)
+                 | otherwise = y: insert x ys
+ 
+ -- b)
+ 
+ isort :: Ord a => [a] -> [a]
+ isort [] = []
+ isort (x:xs) = insert x (isort xs)
