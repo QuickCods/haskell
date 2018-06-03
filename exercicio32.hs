41ed4a97059426a49f7d41bd20eedc4ea09cf613
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-06-03 21:16:31 +0100

    Update exercicio32.hs

--- exercicio32.hs
+++ exercicio32.hs
@@ -1,7 +1,7 @@
+ procura_raiz :: Integer -> Integer -> Integer
+ procura_raiz n r | r^2 == n               = r
+                  | r^2 < n && (r+1)^2 > n = r
+                  | r^2 < n = procura_raiz n ( r+1 )
+
  raizq :: Integer -> Integer
  raizq n = procura_raiz n 0
-      where
-      procura_raiz :: Integer -> Integer -> Integer
-      procura_raiz n r | r^2 == n               = r
-                       | r^2 < n && (r+1)^2 > n = r
-                       | r^2 < n = procura_raiz n ( r+1 )
