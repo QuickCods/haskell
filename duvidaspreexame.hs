a1f9c8877b495830955e3ef089fbdebccc9144c9
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-06-04 15:00:33 +0100

    Update duvidaspreexame.hs

--- duvidaspreexame.hs
+++ duvidaspreexame.hs
@@ -105,7 +105,7 @@
  permutacoes [] = [[]]
  permutacoes (x:xs) = [ps | p <- permutacoes xs, ps <- distribuir x p]
 
- distribuir x [] = [x]
+ distribuir x [] = [[x]]
  distribuir x (y:ys) = (x:y:ys) : (map (y:) (distribuir x ys)) -- [y:xys | xys <- distribuir x ys]
 
  -- recurso
